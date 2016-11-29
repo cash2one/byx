1cb3d2182f09facf7b6a57a1dbc5db0f5f1a5898
陈明 <chenming@ergengtv.com>
2016-11-29 13:44:58 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1@127.0.0.1/byx'
+
+config = {
+    'default': DevelopConfig,
+}
